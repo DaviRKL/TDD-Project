2a6e190719a2a9032647574f31f7ad60c9ccba08
DaviRKL <davirkl07@gmail.com>
2024-07-10 11:41:41 -0300

    Refatorei os testesde caso de uso e criei as models

---
+++ store/models/base.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+import uuid
+from pydantic import Field, BaseModel, UUID4
+
+
+class CreateBaseModel(BaseModel):
+    id: UUID4 = Field(default_factory=uuid.uuid4)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
--- store/usecases/product.py
+++ store/usecases/product.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from store.core.exceptions import NotFoundException
 from store.db.mongo import db_client
+from store.models.product import ProductModel
 from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
 import pymongo
 
@@ -14,10 +15,10 @@ class ProductUsecase:
         self.collection = self.database.get_collection("products")
 
     async def create(self, body: ProductIn) -> ProductOut:
-        product = ProductOut(**body.model_dump())
-        await self.collection.insert_one(product.model_dump())
+        product_model = ProductModel(**body.model_dump())
+        await self.collection.insert_one(product_model.model_dump())
 
-        return product
+        return ProductOut(**product_model.model_dump())
 
     async def get(self, id: UUID) -> ProductOut:
         result = await self.collection.find_one({"id": id})
@@ -38,6 +39,15 @@ class ProductUsecase:
         )
 
         return ProductUpdateOut(**result)
+
+    async def delete(self, io: UUID):
+        product = await self.collection.find_one({"id": id})
+        if not product:
+            raise NotFoundException(message=f"Product not found with filter: {id}")
+
+        result = await self.collection.delete_one({"id": id})
+
+        return True if result.deleted_count > 0 else False
 
 
 product_usecase = ProductUsecase()
